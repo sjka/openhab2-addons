10ff9545e2369764e7fb559fe2ed123a09656bea
Simon Kaufmann <simon.kfm@googlemail.com>
2017-06-01 12:26:44 +0200

    added missing license headers
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- addons/binding/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/handler/Firmware.java
+++ addons/binding/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/handler/Firmware.java
@@ -1,5 +1,16 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.knx.internal.handler;
 
+/**
+ * @author Karel Goderis - Initial contribution
+ */
 public enum Firmware {
     F0(0, "BCU 1, BCU 2, BIM M113"),
     F1(1, "Unidirectional devices"),
